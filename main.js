795c789c2d82b3d9e9771a775a9a6446e0270767
SimpleNX <simplepanigrahi17@gmail.com>
2024-09-02 18:04:18 +0530

    Edit index.html, style.css, main.js
    Edit index.html to add the form to take details.
    Edit style.css to add basic style to the form.
    Edit main.js to add global code for event
    listeners.

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 //The boxes to collect response
-let boxList = document.querySelectorAll(".box");
-
 
 function gameBoard(){
-    const gameboard = [["", "", ""], ["", "", ""], ["", "", ""]];
+    const gameboard = [["S", "S", "S"], ["S", "S", "S"], ["S", "S", "S"]];
     return { gameboard };
 }
 
 function player(name, mark){
-    let name = undefined;
-    let mark = undefined;
     return { name, mark };
 }
 
 function game(){
-    let playerOne = player(name1, mark1);
-    let playerTwo = player(name2, mark2);
+    let playerOne;
+    let playerTwo;
     const players = [playerOne, playerTwo];
 
-    const gameboard = gameBoard();
-    const domoBj = domObj();
+    const board = gameBoard();
     let turn = 0;
     let gameEnd  = false;
     let gameStart = false;
@@ -29,57 +24,29 @@ function game(){
     }
 
     let flipper = (i, j)=>{
-        gameboard[i][j] = players[turn].mark;
+        console.log(players[turn].mark)
+        board.gameboard[i][j] = players[turn].mark;
         turn = flipTurn(turn);
 
         if(!gameEnd)
-            gameEnd = logic(gameBoard);
+            gameEnd = logic(board.gameboard);
         if(gameEnd)
             return true;
         return false;
     };
 
-    return { players, gameboard, turn, flipper, gameStart, flipTurn, reset };
+    return { players, board, turn, flipper, gameStart, flipTurn };
 }
 
 function domObj(gameObj){
     const box = document.querySelector("#ticbox");
     const start = document.querySelector("#start");
-    const dialog = document.querySelector("#det");
+    const dialog = document.querySelector("dialog");
     const reset = document.querySelector("#reset");
     const submit = document.querySelector("#sub");
-    let result = document.querySelector("p #result");
+    const result = document.querySelector("#result");
 
-    box.addEventListener("click", (event)=>{
-        if(gameObj.gameStart){
-            let co = event.target.id;
-            let currBox = document.querySelector(co);
-            let i = co[0], j = co[2];
-            let status = gameObj.flipper(i, j);
-            currBox.innerText = gameObj.gameBoard[i][j];
-            status(status);
-        }
-    });
-    start.addEventListener("click", ()=>{
-        dialog.showModal();
-    });
-    submit.addEventListener("click", (event)=>{
-        let oneName = document.querySelector("#play1 name").value;
-        let oneMark = document.querySelector("#play1 select1").value;
-        let twoName = document.querySelector("#play2 name").value;
-        let twoMark = document.querySelector("#play2 select2").value;
-
-        gameObj.players[0] = { name : oneName, mark : oneMark};
-        gameObj.players[1] = { name : twoName, mark : twoMark};
-
-        event.preventDefault();
-        dialog.close();
-    });
-    reset.addEventListener("click", ()=>{
-        setTimeout(()=>location.reload, 6000);
-    });
-
-    let gameReset = () => setTimeout(()=>location.reload, 6000);
+    let gameReset = () => setTimeout(location.reload(), 6000);
 
     let status = (did)=>{
         if(did){
@@ -87,6 +54,8 @@ function domObj(gameObj){
             gameReset();
         }
     };
+
+    return { box, start, dialog, reset, submit, result, status };
 }
 
 function logic(gameboard){
@@ -95,6 +64,8 @@ function logic(gameboard){
     //horizontal checks
     for(let i=0; i<3; i++){
         let k = gameboard[i][0], c = 1;
+        if(k==="S")
+            continue;
         for(let j = 1; j<3; j++){
             if(gameboard[i][j] === k)
                 c++;
@@ -106,6 +77,8 @@ function logic(gameboard){
     //vertical checks
     for(let i=0; i<3; i++){
         let k = gameboard[0][i], c = 1;
+        if(k==="S")
+            continue;
         for(let j = 1; j<3; j++){
             if(gameboard[j][i] === k)
                 c++;
@@ -117,6 +90,8 @@ function logic(gameboard){
     //diagonal checks
     let c = 0;
     for(let i =0; i<3; i++){
+        if(gameboard[i][i]=='S')
+            break;
         if(gameboard[i][i] === gameboard[0][0])
             c++;
         if(c==3)
@@ -124,6 +99,8 @@ function logic(gameboard){
     }
     let i = 2, j = 0, c2=0;
     while((i>=0 && i<3) && (j>=0 && j<3)){
+        if(gameboard[i][j]==="S")
+            break;
         if(gameboard[i][j]===gameboard[0][2])
             c2++;
         i--;
@@ -134,9 +111,37 @@ function logic(gameboard){
     return false;
 }
 
-function main(){
+const gameObj = game();
+const domobj = domObj(gameObj);
 
-}
+(domobj.box).addEventListener("click", (event)=>{
+    if(gameObj.gameStart){
+        let co = event.target.id;
+        console.log(event.target.innerText);
+        let i = co[0], j = co[2];
+        console.log(i, j)
+        let status = gameObj.flipper(i, j);
+        event.target.innerText = gameObj.board.gameboard[i][j];
+        console.log(event.target.innerText);
+        (domobj.status)(status);
+    }
+});
+(domobj.start).addEventListener("click", ()=>{
+    (domobj.dialog).showModal();
+});
+(domobj.submit).addEventListener("click", (event)=>{
+    let oneName = document.querySelector("#play1 #name").value;
+    let oneMark = document.querySelector("#play1 #select1").value;
+    let twoName = document.querySelector("#play2 #name").value;
+    let twoMark = document.querySelector("#play2 #select2").value;
 
+    gameObj.players[0] = { name : oneName, mark : oneMark};
+    gameObj.players[1] = { name : twoName, mark : twoMark};
 
-main();
+    event.preventDefault();
+    (domobj.dialog).close();
+    gameObj.gameStart = true;
+});
+(domobj.reset).addEventListener("click", ()=>{
+    setTimeout(location.reload(), 5000);
+});
